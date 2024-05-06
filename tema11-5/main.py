3c7b1ecc90a4033ac6c56e1a1832f0051077fdbf
fedyaslonn <133713839+fedyaslonn@users.noreply.github.com>
2024-05-06 21:39:50 +0300

    Add files via upload

--- tema11-5/main.py
+++ tema11-5/main.py
@@ -1,1 +1,34 @@
+'''
+Разработать класс SuperStr, который наследует
+функциональность стандартного типа str и содержит два
+новых метода:
+● метод is_repeatance(s), который принимает некоторую
+строку и возвращает True или False в зависимости от того,
+может ли текущая строка быть получена целым
+количеством повторов строки s. Считать, что пустая
+строка не содержит повторов
+● метод is_palindrom(), который возвращает True или False в
+зависимости от того, является ли строка палиндромом вне
+зависимости от регистра. Пустую строку считать
+палиндромом.
+'''
 
+class SuperStr(str):
+    def is_repeatance(self, s):
+        n = len(self) // len(s)
+        return s * n == self
+
+    def is_palindrome(self):
+        if len(self) == 0:
+            return True
+        if self.lower() == self.lower()[::-1]:
+            return True
+        return False
+
+a = SuperStr('ababa')
+print(a.is_palindrome())
+b = SuperStr('nepalindrom')
+print(b.is_palindrome())
+c = SuperStr('abcabc')
+print(c.is_repeatance('abc'))
+print(c.is_repeatance('av'))
