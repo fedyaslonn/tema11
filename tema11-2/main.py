8bce21b68c10e1200d45144b0532915b52a9c738
fedyaslonn <133713839+fedyaslonn@users.noreply.github.com>
2024-05-06 21:14:33 +0300

    Add files via upload

--- tema11-2/main.py
+++ tema11-2/main.py
@@ -1,1 +1,28 @@
+'''
+Напишите программу с классом Math. При
+инициализации атрибутов нет. Реализовать методы addition,
+subtraction, multiplication и division. При передаче в методы
+двух числовых параметров нужно производить с
+параметрами соответствующие действия и печатать ответ.
+'''
+class Math:
+    @staticmethod
+    def addition(a, b):
+        return f'Сумма чисел {a + b}'
 
+    @staticmethod
+    def substraction(a, b):
+        return f'Рзаница чисел {a - b}'
+
+    @staticmethod
+    def multiplication(a, b):
+        return f'Произведение чисел {a * b}'
+
+    @staticmethod
+    def division(a, b):
+        return f'Деление чисел {a / b}'
+
+print(Math.substraction(10, 5))
+print(Math.addition(19, 3))
+print(Math.multiplication(11, 5))
+print(Math.division(20, 4))
